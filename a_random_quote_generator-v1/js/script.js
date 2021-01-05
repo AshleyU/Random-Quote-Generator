73c44f0273dcf2980c550cc4ad1114141a9d97f4
Ashley U <ashley.umetsu@gmail.com>
2021-01-04 22:00:08 -0600

    Added tags property, random colors, auto refresh and comments
    I added the tags property to one object in the array, and created functions that auto refresh the quotes as well as change bG color

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -3,20 +3,13 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
+// quotes array holding 5 quotes
 
 let quotes = [
 	{
 		quote: "I'm going to make him an offer he can't refuse.",
 		source: "Don Corleone",
 		year: "1972",
-
 	},
 		{
 		quote: "Toto, I've got a feeling we're not in Kansas anymore.",
@@ -26,7 +19,7 @@ let quotes = [
 	{
 		quote: "Louis, I think this is the beginning of a beautiful friendship.",
 		source: "Rick",
-
+		tags: "romance (genre)",
 	},
 	{
 		quote: "As God is my witness, I'll never be hungry again.",
@@ -37,26 +30,28 @@ let quotes = [
 		quote: "Carpe diem. Seize the day, boys. Make your lives extraordinary.",
 		source: "Robin Williams",
 		year: "1989",
+		tags: "drama (genre)",
 
 	},
 
 ];
 
 
-/***
- * `getRandomQuote` function
-***/
+// The getRandomQuote function returns a random quote
 
 function getRandomQuote(arr) {
 	let randomNumber = Math.floor(Math.random() * arr.length);
   	return arr[randomNumber];
 }
 
-/***
- * `printQuote` function
+/*** 
+* The printQuote function calls the getRandomQuote function, 
+* creates an HTML string with conditional statements and 
+* then prints the HTML string to the page
+* calls getRandomColor function & prints color to page
 ***/
 
-function printQuote(arr) {
+function printQuote() {
 	let quoteToPrint = getRandomQuote(quotes);
 	let htmlString = `<p class="quote"> ${quoteToPrint.quote} </p>
 					  <p class="source"> ${quoteToPrint.source}`;
@@ -64,13 +59,37 @@ function printQuote(arr) {
 		htmlString += `<span class="citation"> ${quoteToPrint.citation} </span>`
 	} else if (typeof quoteToPrint.year != "undefined" ) {
 		htmlString += `<span class="year"> ${quoteToPrint.year} </span>`
+	} else if (typeof quoteToPrint.tags != "undefined" ) {
+		htmlString += `<span class="tags"> ${quoteToPrint.tags} </span>`
 	}
+
 	htmlString += "</p>"
-	document.getElementById('quote-box').innerHTML = htmlString; 
+	document.getElementById('quote-box').innerHTML = htmlString;
+	document.body.style.backgroundColor = getRandomColor() 
 	return quoteToPrint;
 }
 
-console.log(printQuote());
+printQuote();
+
+/***
+ * function that randomizes the BG color
+ * code source: https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj
+***/
+
+function getRandomColor() {
+	let randomColor = '#'+ Math.floor(Math.random() * 16777215).toString(16);
+    return randomColor;
+}
+
+// function that auto refreshes the quotes ever 10 seconds
+
+
+function autoRefresh () {
+	let = setInterval(printQuote, 10000)
+}
+
+autoRefresh();
+
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
